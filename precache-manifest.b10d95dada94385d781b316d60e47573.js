bfbd17b130a3ca8c1bd4b91179081ad62a332b59
nitin.babu <nitin.babu@snapdeal.com>
2020-08-18 21:20:29 +0530

    Trigger rebuild

---
+++ precache-manifest.b10d95dada94385d781b316d60e47573.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac0c254ec14cfc1ed93b8ce8016b073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "941be908c64f765ce8b7",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "77de1cb00ec2530a7c6d",
+    "url": "/static/css/main.27ecdb0d.chunk.css"
+  },
+  {
+    "revision": "941be908c64f765ce8b7",
+    "url": "/static/js/2.93bdd741.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.93bdd741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77de1cb00ec2530a7c6d",
+    "url": "/static/js/main.16047797.chunk.js"
+  },
+  {
+    "revision": "020a87e7c106589a5cba",
+    "url": "/static/js/runtime-main.45de4e5c.js"
+  },
+  {
+    "revision": "977ecf7be74437c437106e00ad13f379",
+    "url": "/static/media/Bitmap-autochess.977ecf7b.png"
+  },
+  {
+    "revision": "7173cfe7840a5f2d3853b4220f6cf851",
+    "url": "/static/media/Bitmap-garenafreefire.7173cfe7.png"
+  },
+  {
+    "revision": "90640a8778af0217304f5effb1aa8413",
+    "url": "/static/media/Bitmap-mortalkombat.90640a87.png"
+  },
+  {
+    "revision": "c161ba4abb12e86093bc396fd2337b9d",
+    "url": "/static/media/Bitmap-needforspeed.c161ba4a.png"
+  },
+  {
+    "revision": "e51f42c349763884e809677c9634b288",
+    "url": "/static/media/Bitmap-pubgmobile.e51f42c3.png"
+  },
+  {
+    "revision": "8a4d68dbcc8a535baeeef974e67aaed2",
+    "url": "/static/media/Bitmap-shadowfight.8a4d68db.png"
+  },
+  {
+    "revision": "3ecfc5bd32f7be1a974631a1e152d0b3",
+    "url": "/static/media/Bitmap-summonerswar.3ecfc5bd.png"
+  }
+]);
